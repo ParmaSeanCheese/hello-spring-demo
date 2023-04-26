1b02bc5be625b134f782ad3fc32f7927366afe9c
Sean <Seanpf12@hotmail.com>
2023-04-26 10:53:41 -0500

    Finished Exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,60 +1,39 @@
 package org.launchcode.hellospring.controllers;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
-
-    // Handles request at path /hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello(){
-//        return "Hello, Spring!";
-//    }
-
-    // lives /hello/goodbye due to overarching class annotation
-    @GetMapping("goodbye")
-    public String goodbye(){
-        return "Goodbye, Spring!";
-    }
 
     // Handles request of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello.html";
     }
 
     // Handles requests of the form /hello/LaunchCode
     @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model){
+        model.addAttribute("greeting", "Hello, "  + name + "!");
+        return "hello.html";
     }
 
     //lives at /hello/form
     @GetMapping("form")
     public String helloForm(){
-        return "<html>" +
-                "<body>" +
-                    "<form action='/hello/greeting' method='post'>" + //submit a request to /hello
-                        "<input type='text' name='name'>" +
-                        "<select name='language' id='language'>" +
-                            "<option value=''>--Please select Language--</option>" +
-                            "<option value='english'>English</option>" +
-                            "<option value='french'>French</option>" +
-                            "<option value='spanish'>Spanish</option>" +
-                            "<option value='german'>German</option>" +
-                            "<option value='russian'>Russian</option>" +
-                        "</select>" +
-                        "<input type='submit' value='Greet me!'>" +
-                    "</form>" +
-                "</body>" +
-                "</html>";
+        return "form.html";
     }
 
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "greeting")
+    @ResponseBody
     public String createMessage(@RequestParam String name, @RequestParam String language){
         if (language.equals("")){
             return "Please Select a language";
@@ -68,6 +47,16 @@ public class HelloController {
             return "Guten Tag, " + name + "!";
         }
         return "Привет, " + name + "!";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list.html";
     }
 
 }
